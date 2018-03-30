76dcef06ef5488b1fbe989f33d841915e8067feb
montekaka <josh.chen.jia@gmail.com>
2018-03-30 08:24:53 -0700

    init the project

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+  var docBody = document.body;
+  //console.log(docBody);
+
+ //  if(docBody.classList) {
+ //  	if(docBody.classList.value === className){
+	// 		result.push(docBody);
+ //  	}  	
+ //  } 
+	// var nodes = docBody.childNodes;
+ //  for(var i = 0; i < nodes.length; i++) {
+ //  	if(nodes[i].classList && (nodes[i].classList.value.split(" ")).includes(className) ) {
+ //  		result.push(nodes[i]);  		
+ //  		//console.log(nodes[i])
+ //  	}  	
+ //  }  
+ //  //console.log(result)
+ //  return result;
+
 };
