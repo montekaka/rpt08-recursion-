ff1dbdcb7ded360c8dd3e77b6c4c3a0148ed4756
montekaka <josh.chen.jia@gmail.com>
2018-04-07 22:34:02 -0700

    implement the method to take care basic stuff object

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,52 +2,178 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
 var parseJSON = function(json) {
   // your code goes here
-  findTheClosure = function(str, openClsureStr){
-    var closureStr = "]";
-    if(openClsureStr === "{"){
-      closureStr = "}";
-    }
-    var i = 1;
-    while(str[i] !== closureStr && i < str.length) {
-      i += 1;
-    }    
-    if(str[i] === closureStr) {
-      return i;
-    } else {
-      return -1;
-    }
-  }
-  getValue = function(str, fromIdx, toIdx) {
-    var n = toIdx - fromIdx - 1;
-    if(n > 0){
-      return str.split('').splice(fromIdx + 1, n).join('');
-    }  
-  }
-  createObj = function(openClsureStr){
-    if(openClsureStr === "["){
-      return [];
-    } else if(openClsureStr === "{") {
+  // JSON Grammer
+  // determine if the str is object, array, or elements 
+  var result = setResultVar(json);
+  var result_type = getJSONType(json);
+
+  if(result) {
+    var json = remvoeBracket(json);  
+    var jsonStrs = parseStr(json);   
+    result = setupObject(jsonStrs, result);
+    return result;    
+  } else {
+    return json;
+  }  
+  return result;
+
+};
+
+
+ var setResultVar = function(str){
+    if(str[0]==="{") {
       return {};
-    } else {
-      return -1;
+    } else if(str[0]==="["){
+      return [];
     }
   }
 
-  var openClsureStr = json[0];
-  var result = createObj(openClsureStr);
-  if(result !== -1){
-    var s = Object.prototype.toString;
-    var fromIdx = 0;
-    var toIdx = findTheClosure(json, openClsureStr);
-    var value = getValue(json,fromIdx, toIdx);
-    if(value){
-      if(s.call(result) === "[object Array]") {
-        result.push(value);
-        return result;
-      }
+  var getJSONType = function(str){
+    if(str[0]==="{") {
+      return 'object';
+    } else if(str[0]==="["){
+      return 'array';
+    } else {
+      return 'value';
     }
-    return result;
-  }  
-};
+  }
+
+  var getStrValue = function(str) {
+    if(str === 'undefined') {
+      return undefined;
+    } else if(str === 'null') {
+      return null;
+    } else if(str === 'true') {
+      return true;
+    } else if(str === 'false') {
+      return false;
+    } else if (str === '') {
+      return str;
+    }
+    else if(isNaN(Number(str))) {
+      return str;
+    } else {
+      return Number(str)
+    }
+  }
+
+var remvoeBracket = function(str){
+  var strs = str.split('');
+  strs.splice(0,1)    
+  strs.splice( strs.length -1, 1);  
+  str = strs.join('');
+  return str;
+}
+
+var quotationReducer = function(quotationCount, cb){
+  if(quotationCount === 2) {
+    cb(0);
+    return 0;
+  } else {
+    cb(quotationCount);
+    return quotationCount;
+  }
+}
+
+var getSimpleString = function(ele, strEles) {
+  strEles.push(ele);
+}
+
+var getStringBetweenQuotations = function(quotationCount, ele, strEles, result){  
+  if(quotationCount > 0) {    
+    if(ele !== "\""){
+      strEles.push(ele);
+    }    
+  } else {
+    result.push(strEles.join(""));
+    strEles = [];
+  }    
+
+  return strEles;
+}
+
+var updateStrsWithoutQuotations = function(quotationCount, strEles, result){
+  if(strEles.length > 0){
+    result.push(strEles.join(""));
+    return strEles = [];
+  }
+  return strEles;
+}
+
+var removePunctuation = function(str) {
+  var str = str.split(',').join('');
+  return str.split(":").join('');
+}
+
+
+var parseStr = function(str) {
+  var resultStrs =[];
+  var strEles = [];
+  var strEleswoq = []; // for the one without quotation marks
+  var quotationCount = 0;
+  var strs = str.split('');
+  for(var i = 0; i < strs.length; i++) {
+    var char = strs[i];
+    if(char === '\"'){
+      quotationCount += 1;
+    }
+    quotationCount = quotationReducer(quotationCount, function(quotation_count){            
+      if(quotationCount > 0) {
+        strEles = getStringBetweenQuotations(quotation_count, char, strEles, resultStrs);        
+        if(strEleswoq.length > 0 ) {
+          //resultStrs.push(removePunctuation(strEleswoq.join('')))
+          addToResultArr(resultStrs, removePunctuation(strEleswoq.join('')));
+        }
+        strEleswoq = [];
+      } else {          
+        getSimpleString(char, strEleswoq);     
+      }
+    });
+    if(i === strs.length - 1 && strEleswoq.length > 0 ){
+      //resultStrs.push(removePunctuation(strEleswoq.join('')));  
+      addToResultArr(resultStrs, removePunctuation(strEleswoq.join('')));
+    }
+  }
+  return resultStrs;
+}
+
+var removeLeadingSpace = function(str){
+  if(str){
+    if(str[0] === " "){
+      var strs = str.split('');
+      strs.splice(0,1);    
+      str = strs.join('');      
+    }
+    var n = str.split('').length;
+    var n = n - 1;
+    if(str[n] === " "){
+      var strs = str.split('');
+      strs.splice(n,1);    
+      str = strs.join('');      
+    }  
+    return str;  
+  }
+  return str;
+}
+
+var setupObject = function(strs, obj) {
+  var i = 0;
+  while(i < strs.length){
+    var key = strs[i];
+    var val = removeLeadingSpace(strs[(i+1)]); 
+    obj[key] = getStrValue(val);
+    i += 2;
+  }
+  return obj;
+}
+
+var addToResultArr = function(array, cb){
+  
+  var ele = removeLeadingSpace(cb);
+  if(ele.length > 0 ){
+    array.push(ele);
+  }
+}
